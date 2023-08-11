c98920ef286b9bcc492bc946b9491e7b78925898
Robin Sardja <robinsardja@gmail.com>
2023-08-11 18:29:37 -0400

    Added swiping to navigate between routes. https://stackoverflow.com/questions/55050463/how-to-detect-swipe-in-flutter

--- lib/camera/camera_route.dart
+++ lib/camera/camera_route.dart
@@ -27,7 +27,7 @@ class _CameraRouteState extends State<CameraRoute> {
   String? _text;
   var _cameraLensDirection = CameraLensDirection.back; // TO DO: use camera lens direction from camDesc
 
-  // TO DO: change route based on selected index
+  // handles nav bar changing routes
   int _selectedIndex = 1;
   void _changeIndex(int index) {
     if( index == _selectedIndex ) return;
@@ -87,37 +87,52 @@ class _CameraRouteState extends State<CameraRoute> {
       DeviceOrientation.portraitDown
     ]);
     // show loading screen while initializing camera
-    return Scaffold(
-      appBar: AppBar( title: const Text('PowerVAR') ),
-      body: Center(
-        child: DetectorView(
-          title: "",
-          customPaint: _customPaint,
-          text: _text,
-          onImage: _detectPoses,
-          initialCameraLensDirection: _cameraLensDirection,
-          onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+    return SizedBox.expand(
+      child: GestureDetector( // swiping to navigate between routes
+        onPanUpdate: (details) {
+          if( details.delta.dx > 0 ) { // swipe right
+            _changeIndex( _selectedIndex - 1);
+          }
+          if( details.delta.dx < 0 ) { // swipe left
+          _changeIndex( _selectedIndex + 1);
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('PowerVAR'),
+            automaticallyImplyLeading: false,
+          ),
+          body: Center(
+            child: DetectorView(
+              title: "",
+              customPaint: _customPaint,
+              text: _text,
+              onImage: _detectPoses,
+              initialCameraLensDirection: _cameraLensDirection,
+              onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+            ),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.photo_camera),
+                label: 'Camera',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              )
+            ],
+            selectedFontSize: 0, // hide icon label
+            iconSize: 32, // enlargen icon size
+            currentIndex: _selectedIndex,
+            onTap: (index) => _changeIndex(index),
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.photo_camera),
-            label: 'Camera',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          )
-        ],
-        selectedFontSize: 0, // hide icon label
-        iconSize: 32, // enlargen icon size
-        currentIndex: _selectedIndex,
-        onTap: (index) => _changeIndex(index),
       ),
     );
   }
--- lib/settings/settings_route.dart
+++ lib/settings/settings_route.dart
@@ -9,8 +9,63 @@ class SettingsRoute extends StatefulWidget {
 
 class _SettingsRouteState extends State<SettingsRoute> {
 
+  // handles nav bar changing routes
+  int _selectedIndex = 2;
+  void _changeIndex(int index) {
+    if( index == _selectedIndex ) return;
+    setState( () {_selectedIndex = index;} );
+    switch( index ) {
+      case 0:
+        Navigator.pushNamed( context, "Home" );
+        break;
+      case 1:
+        Navigator.pushNamed( context, "Camera" );
+        break;
+      case 2:
+        Navigator.pushNamed( context, "Settings" );
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(); // 
+    return SizedBox.expand(
+      child: GestureDetector(
+        onPanUpdate: (details) {
+          if( details.delta.dx > 0 ) { // swipe right
+            _changeIndex( _selectedIndex - 1);
+          }
+          if( details.delta.dx < 0 ) { // swipe left
+          _changeIndex( _selectedIndex + 1);
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text( "Settings" ),
+            automaticallyImplyLeading: false,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.photo_camera),
+                label: 'Camera',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              )
+            ],
+            selectedFontSize: 0, // hide icon label
+            iconSize: 32, // enlargen icon size
+            currentIndex: _selectedIndex,
+            onTap: (index) => _changeIndex(index),
+          ),
+      ),
+      ),
+    );
   }
 }
