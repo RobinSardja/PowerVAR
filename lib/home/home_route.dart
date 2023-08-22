01fbac2fa3ef87b2a5bf8d1c50ec1239c3506c96
Robin Sardja <robinsardja@gmail.com>
2023-08-21 21:52:39 -0400

    Beginning UI design of home route.

--- lib/home/home_route.dart
+++ lib/home/home_route.dart
@@ -9,6 +9,13 @@ class HomeRoute extends StatefulWidget {
 
 class _HomeRouteState extends State<HomeRoute> {
 
+  static const _imageWidth = 250.0;
+  static const _minButtonWidth = 200.0;
+  static const _minButtonHeight = 25.0;
+  static const _buttonStyle = ButtonStyle(
+    minimumSize: MaterialStatePropertyAll( Size( _minButtonWidth, _minButtonHeight ) ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -16,20 +23,27 @@ class _HomeRouteState extends State<HomeRoute> {
         mainAxisSize: MainAxisSize.min,
         children: [
           const Text( "PowerVAR" ),
-          Image.asset( "assets/home icon.png" ),
+          Image.asset(
+            "assets/home icon.png",
+            width: _imageWidth,
+          ),
           ElevatedButton(
+            style: _buttonStyle,
             onPressed: () {},
             child: const Text( "New Lift" ),
           ),
           ElevatedButton(
+            style: _buttonStyle,
             onPressed: () {},
             child: const Text( "Settings" ),
           ),
           ElevatedButton(
+            style: _buttonStyle,
             onPressed: () {},
             child: const Text( "Support" ),
           ),
           ElevatedButton(
+            style: _buttonStyle,
             onPressed: () {},
             child: const Text( "Help and About" ),
           ),
